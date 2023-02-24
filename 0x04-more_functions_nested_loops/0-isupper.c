a6f54ef9413639a013f3121356b101ccff9ca073
David Olaleye <olaoluwaolaleye@gmail.com>
2023-02-24 03:17:53 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isupper - check if letter is uppercase
+*@c: The parameter
+*Return: 1 for uppercase 0 lowercase
+*/
+
+int _isupper(int c)
+{
+	if (x >= 65 && <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
