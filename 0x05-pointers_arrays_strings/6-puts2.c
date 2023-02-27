22b1a9ef544d3c2332a3911511b26fe1c899cd22
David Olaleye <olaoluwaolaleye@gmail.com>
2023-02-27 10:39:37 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*puts2 - Print other character of string
+*
+*@str: parameter
+*Return: null
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
